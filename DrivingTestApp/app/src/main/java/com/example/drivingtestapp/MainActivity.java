4ca226ef1f1d71fa5721cffa9fa754ee7c62fe8a
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-11-01 17:39:49 +0700

    Added NavigationDrawer and declared buttons in MainActivity class

--- DrivingTestApp/app/src/main/java/com/example/drivingtestapp/MainActivity.java
+++ DrivingTestApp/app/src/main/java/com/example/drivingtestapp/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.drivingtestapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-public class MainActivity extends AppCompatActivity {
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    Button btnExam, btnLearn, btnSign, btnLaw;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout_main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnExam = findViewById(R.id.btnExam);
+        btnLearn = findViewById(R.id.btnLearn);
+        btnSign = findViewById(R.id.btnSign);
+        btnLaw = findViewById(R.id.btnLawDetail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout_main);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_main) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_thoat) {
+            exit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void exit() {
+        finishAffinity();
+        System.exit(0);
+    }
+
 }
